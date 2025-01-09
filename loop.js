971d55d0f3ee241568dcef1ea09e4485cff5603e
Robert-Lexicon <robert.hedblad@lexicon.se>
2025-01-09 11:54:28 +0100

    Loops
    Co-authored-by: Tony Berglund <tony_berglund@outlook.com>

---
+++ loop.js
@@ -0,0 +1,32 @@
+//for loop
+for (let i = 0; i <= 4; i++) {
+  console.log(i);
+}
+
+//while loop
+let j = 0;
+while (j <= 4) {
+  console.log("pre:" + j);
+  j++;
+  console.log("post:" + j);
+
+  // if(j===2) {
+  //   break;
+  // }
+}
+
+//loop
+const nrs = [1, 2, 4, 8]; //array
+
+for (const i in nrs) {
+  console.log(i);
+}
+for (const nr of nrs) {
+  console.log(nr);
+}
+
+//object key - value
+const item = { color: "green", opacity: 0.7, visible: true };
+for (const [key, value] of Object.entries(item)) {
+  console.log(key + ":" + value);
+}
